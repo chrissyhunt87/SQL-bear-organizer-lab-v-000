27b797eb563bb13c219b9fa081179a59017b4630
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-11-29 19:34:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,9 +19,8 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  brown = "SELECT color, COUNT(color) FROM bears WHERE color = 'brown'"
-  black = "SELECT color, COUNT(color) FROM bears WHERE color = 'black'"
-  brown > black ? brown : black
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'brown'"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'black'"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
